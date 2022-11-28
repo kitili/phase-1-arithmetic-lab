e5442fa103226dcb6f2892ebbb4eeed7c209618c
kitili <you@example.com>
2022-11-28 05:20:45 -0800

    dfgnm

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = -31;
+let num2 = -2;
+let num3 = 20;
+let num4 = 16;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max = Math.max(num1, num2, num3, num4);
